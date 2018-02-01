e04cc20691252063ca10b60b7f2af11815fcfea8
Charles Mitchell <charlesmitchell@protonmail.com>
2018-02-01 15:42:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,6 +11,6 @@ var updatePlaylist = function(playlist, artistName, songTitle){
 var removeFromPlaylist = function(playlist, artistName){
   
   delete playlist.artistName; 
-  return playlist;
+
   
 }
